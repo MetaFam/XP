f15d0197ecc4afa0c29ad6b746c89503050677b3
Michiel Quellhorst <mquellhorst@protonmail.com>
2021-12-14 22:29:35 +0100

    preperations for mint
    * Reduced GitHub weight
    * Merged accounts
    * Deactivated non-players
    * Added initiative for Davor for running design guild

--- scripts/seed-minting.js
+++ scripts/seed-minting.js
@@ -45,10 +45,10 @@ const numberToWei = (n) => web3.utils.toWei(parseFloat(n).toFixed(9), 'ether');
 
 
 const MINT_TX_HASH = "https://polygonscan.com/tx/0xc67d316e622245603ebfaec12794e86d88016e25f367ae22366c4c5e4494d371";
-const MINT_DATE = "Nov 22 2021";
+const MINT_DATE = "Dec 21 2021";
 
 const LEDGER_PATH = 'data/ledger.json';
-const MINT_AMOUNTS_PATH = './scripts/toMint11Merkle.json';
+const MINT_AMOUNTS_PATH = './scripts/toMint12Merkle.json';
 const ETH_MAIN_NET_IDENTITY_ID = "igdEDIOoos50r4YUKKRQxg";
 
 async function deductSeedsAlreadyMinted(accounts, ledger) {
