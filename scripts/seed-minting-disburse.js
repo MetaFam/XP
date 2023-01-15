27b496a405b0acb089a784d7d28c0219417e9c59
luxumbra <me@luxumbra.dev>
2023-01-15 17:15:08 +0000

    December distribution

--- scripts/seed-minting-disburse.js
+++ scripts/seed-minting-disburse.js
@@ -17,7 +17,7 @@ const MINT_TX_HASH = "https://polygonscan.com/tx/0x4e544370a25dda862278631f0f492
 const MINT_DATE = "Dec 5 2022";
 
 const LEDGER_PATH = 'data/ledger.json';
-const MINT_AMOUNTS_PATH = './scripts/toMint20Disburse.json';
+const MINT_AMOUNTS_PATH = './scripts/toMint21Disburse.json';
 const ETH_MAIN_NET_IDENTITY_ID = "igdEDIOoos50r4YUKKRQxg";
 
 async function deductSeedsAlreadyMinted(accounts, ledger) {
@@ -93,6 +93,12 @@ async function deductSeedsAlreadyMinted(accounts, ledger) {
   let total = 0;
   accountsWithAddress.forEach(acc => {
     const amountToMint = G.format(acc.balance, 9, '');
+
+    // ignore players with zero to mint. They just get removed from the csv later anyway.
+    if (parseFloat(amountToMint) <= parseFloat(0.000000000)) {
+      return null;
+    }
+
     newMintAmounts[acc.ethAddress] = amountToMint;
     if (!isValidAddress(acc.ethAddress)) {
       console.log('INVALID ADD for acc: ', acc);
