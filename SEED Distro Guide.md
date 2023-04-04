f651cf82a1f6500980dc12a9839b6ab8b6e7eb3e
δυς <dys@dhappy.org>
2023-04-04 11:42:28 -0400

    WIP: Ledger Deduction #23 & some fixes (#154)
    * moving distro data files from `scripts` folder 🦠
    
    * adding command line options & renaming outputs 🥅
    
    * removing debug statements & outputting total disbursed 🍤
    
    * renaming files & prepping to check output 🎐
    
    * handling decimals correctly 🚰
    
    * max fraction digits was too high 🔮
    
    * writing the disbursal to the ledger 🖖🏿

---
+++ SEED Distro Guide.md
@@ -0,0 +1,51 @@
+
+# How to Do SEED Distributions
+
+## A. Calculating distribution + updating ledger
+1. Fork/clone the repo locally and follow the instructions in the main readme to load the SourceCred instance (you will need API keys for Github and Discord)
+2. If you already forked the repo, make sure to pull any changes from the master branch (since the discord bot updates ETH addresses in the ledger programmatically)
+3. Due to painfully long and unreliable method of loading contribution data locally using `yarn load`, we now use the following method from within the root of the XP repo, [as advised by SC](https://discord.com/channels/453243919774253079/718263631158050896/778428725570174986):
+    1. On `master`, fetch the latest updates from origin: `git pull`.
+    2. Checkout the `output` directory of the `gh-pages` branch: `git checkout origin/gh-pages -- output` — *(the graph data from the last run)*.
+    3. Run `yarn serve` to fire up the interface from [xp.metagame.wtf](https://xp.metagame.wtf) with the latest data.
+4. Open the admin interface at [`localhost:6006`](http://localhost:6006) and go to the “Identities” tab. To merge accounts:
+    1. Search in 'Filter List' for the accounts to be merged.
+    2. For each user, use the “Add Alias” field to search for their aliases.
+    3. Click on all the accounts needing to be merged into the selected user.
+    4. Repeat the process for any other users.
+    5. Once you are done, click “Save ledger to disk”.
+    6. Exit the running SC instance *(`CTRL+C` in terminal)*.
+5. *If you do need to activate players, you will want to commit the ledger, push changes and create a PR before progressing with Step 6, otherwise skip the rest of this step.* Create a new branch along the lines of `git checkout -b activations/year-month-day`, commit changes to `ledger.json`, push, and open a PR. Once merged, we need to wait overnight for the runners to run again with the newly activated or merged players.
+> ❢ If you activated players, confirm the GitHub actions have run *(sometimes they fail)* since your PR was merged. You will need to repeat Step 3 again to get the latest data before moving to Step 7 .
+6. Check [this Observable notebook](https://observablehq.com/@hammadj/metagame-active-contributors) to see how many `seedsToMintForCurrentInterval`. This is based on 20 SEEDs per week per active contributor *(where “active” players earned at least 20 XP)*.
+7. Update the `config/grain.json` file with ~75% of the `seedsToMint` amount going to `RECENT` policy and ~25% going to the `BALANCED` policy *(e.g. if the number in Observable is 400, put 300 in Recent and 100 in Balanced)*.
+8. Check the leaderboard in the UI to ensure the XP amounts for the distribution period make sense.
+9. In a new terminal window, run `yarn grain` to do the distribution.
+10. Refresh the admin page and go to the SEED Accounts page to see the new SEED balance for each player, ensure these amounts make sense.
+11. Create a new branch at this point and name it something sane like `git checkout -b dist/day-month–day-month-year`.
+12. Run `./scripts/seed-minting-disburse.mjs` in a terminal to generate the JSON & CSV files of ETH addresses and SEED amounts to disburse. *(The script will use the first file constructed using the `output-pattern` argument that doesn't exist for output.)*
+13. Keep track of the total amount of SEEDs to disburse. This value will be used later when verifying data for deducting from the ledger in Step C.
+14. Post the distribution to the community *(to #seed-minting)* using [this Google Sheet](https://docs.google.com/spreadsheets/d/1m8XGjFnTpozt5BBlCZgHen09msimS3HHIT2Sb5Shuro/) *(ask @luxumbra for edit access)*. Refer to previous sheets for formatting convention. We usually give 72 hours to raise issues. If you need to re-run a distribution, `git checkout ledger.json` before running `yarn grain` again.
+15. Once discussion has concluded, commit and push all changes to Github, and make a PR to merge them into `master`.
+> ⚠️ If there are merge conflicts, you may need to run `scripts/rebase-ledger.js`.
+
+## B. Airdrop Distribution
+> ⚠️ *This step requires you to be a signer on the [Polygon multisig](https://gnosis-safe.io/app/matic:0xbaF60086Da36033B458B892e2432958e219F4Ed6).*
+
+1. Reformat the final [Google Sheet](https://docs.google.com/spreadsheets/d/1m8XGjFnTpozt5BBlCZgHen09msimS3HHIT2Sb5Shuro/) to match the same headings & columns as seen in `distros/Disbursal #23.SourceCred Airdrop.csv`. **Note the addition of the leading  `token_type` (`erc20`) and `token_address` (`0xEAeCC18198a475c921B24b8A6c1C1f0f5F3F7EA0`) columns.** 2. Go to [the gnosis safe](https://gnosis-safe.io/app/matic:0xbaF60086Da36033B458B892e2432958e219F4Ed6) and sign in. Head to Apps → CSV Airdrop and upload the `.csv` file from the previous step. Next, you should see the contents of the file in the textfield.
+3. Verify the data looks good, submit, and sign the transaction.
+4. Once the transaction is confirmed, grab the transaction URL, and post it in #multisig, pestering the signers until it's done.
+5. Relax… until the drop is done and you then need to move to step C — as soon as possible post distribution (after your distro branch has been merged) to avoid any ledger conflicts!
+
+## C. Deducting Minted SEEDs from the Ledger
+
+1. Pull down the latest changes from origin (with data from the latest dist)
+2. Create a new branch from master and give it a sensible name eg: `deduction/#23`
+3. Rerun `./scripts/seed-minting-disburse.js` with the following arguments:
+   1. Pass `--tx-url=` as the Polygonscan link for the `seedAllocations` transaction from step A.10 above.
+   2. Pass `--tx-date=` as the date of the said transaction
+   3. Pass `-o` to cause the script to overwrite the last disbursal.
+4. Run something akin to `./scripts/seed-minting-disburse.js --tx-url=https://polygonscan… --tx-date=2023-03-15 -o`, and ensure there's no errors or “Extra SEED Balance” messages logged.
+5. Ensure that the `distros/Disbursal #${index}.SourceCred.*` files for the current distribution did not have any changes. This ensures that the on chain distribution exactly matches the ledger state.
+6. Run `yarn serve`, and double check that the current balance in SEED Accounts is 0 for people in the distribution.
+7. Commit the updates to `ledger.json`, push them, and make a new PR to `master`.
---
+++ scripts/seed-minting-disburse.mjs
@@ -0,0 +1,255 @@
+#!/usr/bin/env node
+
+import { sourcecred as sc } from 'sourcecred'
+import fs from 'fs-extra'
+// import { isValidAddress } from 'web3-utils'
+import _ from 'lodash'
+import yargs from 'yargs'
+
+// ToDo: Replace with working implementation
+const isValidAddress = (addr) => true
+
+const processArgs = async (defaults = {
+  txUrl: null,
+  txDate: null,
+  overwrite: false,
+  ledger: 'data/ledger.json',
+  outputPattern: 'distros/Disbursal #${index}.SourceCred',
+  mainnetId: 'igdEDIOoos50r4YUKKRQxg',
+  dryRun: false,
+}) => {
+  const args = yargs(process.argv.slice(2))
+  .usage(
+    '$0 [options]',
+    'Generate disbursal JSON/CSV to files based on'
+    + ' `output-pattern`. If `tx-url` & `tx-date` &'
+    + ' `overwrite` are provided, then the most recently'
+    + ' generated disbursal will be overwritten, and that'
+    + ' disbursal will be written to the ledger.',
+  )
+  .option('tx-url', {
+    type: 'string',
+    default: process.env.SC_TX_URL ?? defaults.txUrl,
+    alias: 'u',
+    description: (
+      'URL for the disbursal transaction on the Polygonscan (`SC_TX_URL`)'
+    ),
+  })
+  .option('tx-date', {
+    type: 'string',
+    default: process.env.SC_TX_DATE ?? defaults.txDate,
+    alias: 'd',
+    description: (
+      'Date of the disbursal transaction (`SC_TX_DATE`)'
+    ),
+  })
+  .option('ledger', {
+    type: 'string',
+    default: process.env.SC_LEDGER ?? defaults.ledger,
+    alias: 'l',
+    description: 'Path of `ledger.json` (`SC_LEDGER`)'
+  })
+  .option('overwrite', {
+    type: 'boolean',
+    default: process.env.SC_OVERWRITE ?? defaults.overwrite,
+    alias: 'o',
+    description: 'Overwrite the last disbursal (`SC_OVERWRITE`)'
+  })
+  .option('output-pattern', {
+    type: 'string',
+    default: (
+      process.env.SC_DISBURSAL ?? defaults.outputPattern
+    ),
+    alias: 'p',
+    description: 'Path to generate JSON/CSV to (`SC_DISBURSAL`)'
+  })
+  .option('mainnet-id', {
+    type: 'string',
+    default: process.env.SC_MAINNET_ID ?? 'igdEDIOoos50r4YUKKRQxg',
+    alias: 'm',
+    description: 'Something to do with the mainnet id (`SC_MAINNET_ID`)'
+  })
+  .option('dry-run', {
+    type: 'boolean',
+    default: false,
+    alias: 'n',
+    description: 'Don’t write anything to disk'
+  })
+  .alias('h', 'help')
+  .help()
+  .showHelpOnFail(true, 'HELP!')
+
+  return args.argv
+}
+const argv = await processArgs()
+
+if((argv.txUrl && !argv.txDate) || (!argv.txUrl && argv.txDate)) {
+  console.error('Must provide `--tx-date` & `--tx-url` to write to ledger.')
+  console.error(' Or neither to write CSV & JSON.')
+  process.exit(13)
+}
+
+const overwrite = (
+  argv.overwrite && argv.txUrl != null && argv.txDate != null
+)
+
+let index = 23 // index as of 2023/03/29
+let base, prev
+do {
+  prev = base
+  base = argv.outputPattern.replace(
+    /\${index}/g, (index++).toString()
+  )
+} while(fs.existsSync(`${base}.json`))
+
+const outputBase = overwrite ? prev : base
+
+const Ledger = sc.ledger.ledger.Ledger
+const G = sc.ledger.grain
+
+const NodeAddress = sc.core.address.makeAddressModule({
+  name: 'NodeAddress',
+  nonce: 'N',
+  otherNonces: new Map().set('E', 'EdgeAddress'),
+})
+
+async function deductSeedsAlreadyMinted({ accounts, ledger }) {
+  const lastDisburse = JSON.parse((await fs.readFile(`${outputBase}.json`)).toString())
+
+  let total = 0n
+
+  Object.entries(lastDisburse).forEach(([address, amount]) => {
+    const account = accounts.find(({ ethAddress: a }) => (
+      a.toLowerCase() === address.toLowerCase()
+    ))
+
+    if(!account) {
+      console.warn(`Missing account for: ${address}`)
+      return
+    }
+
+    const disbursement = G.fromApproximateFloat(amount)
+    const balance = G.fromString(account.balance)
+    // console.debug({ seedsBalance, seedsMinted, mint })
+    // console.debug({ address, amount, seedsMinted })
+
+    let transfer = disbursement
+    // Only transfer up to max balance
+    if(G.lt(balance, disbursement)) {
+      console.warn(
+        `Extra SEED Balance for: ${account.ethAddress}:`
+        + ` -${G.sub(disbursement, balance)}`
+      )
+      transfer = balance
+    }
+
+    if(transfer > 0) {
+      ledger.transferGrain({
+        from: account.identity.id,
+        to: argv.mainnetId,
+        amount: transfer,
+        memo: (
+          `Disbursed SEED on Polygon to ${account.ethAddress}`
+          + ` on ${argv.txDate} (${argv.txUrl})`
+        ),
+      })
+      total += BigInt(transfer)
+    }
+  })
+  console.info({ 'total disbursed': total })
+}
+
+(async () => {
+  const ledgerJSON = (await fs.readFile(argv.ledger)).toString()
+
+  const ledger = Ledger.parse(ledgerJSON)
+  const accounts = ledger.accounts()
+
+  const accountsWithAddress = accounts.map((a) => {
+    if(a.identity.subtype === 'BOT') return null
+    if(!a.active) return null
+
+    const ethAliases = a.identity.aliases.filter(
+      (alias) => {
+        const parts = NodeAddress.toParts(alias.address)
+        return parts.includes('ethereum')
+      }
+    )
+
+    if(ethAliases.length === 0) return null
+
+    let ethAddress = null
+
+    ethAliases.forEach((alias) => {
+      ethAddress = NodeAddress.toParts(alias.address)[2]
+    })
+
+    return { ...a, ethAddress }
+  }).filter(Boolean)
+
+  if(overwrite) {
+    await deductSeedsAlreadyMinted({
+      accounts: [...accountsWithAddress],
+      ledger,
+    })
+    console.info(`Writing ledger to: ${argv.ledger}.`)
+    if(argv.dryRun) {
+      console.info('𝙳𝚛𝚢 𝚁𝚞𝚗: Not writing ledger.')
+    } else {
+      await fs.writeFile(argv.ledger, ledger.serialize())
+    }
+  }
+
+  const addressAccounts = (
+    _.keyBy(accountsWithAddress, 'ethAddress')
+  )
+  const newMintAmounts = {}
+  let total = 0
+  accountsWithAddress.forEach((acc) => {
+    const amountToMint = Number(G.format(acc.balance, 9, ''))
+
+    // ignore players with zero to mint. They just get removed from the csv later anyway.
+    if(amountToMint <= 0) return
+
+    newMintAmounts[acc.ethAddress] = amountToMint
+   
+    if(!isValidAddress(acc.ethAddress)) {
+      console.warn(`INVALID ADDR for acct: ${acc}`)
+    }
+
+    total += amountToMint
+  })
+
+  const csv = (
+    Object.entries(newMintAmounts).map(([address, amount]) => {
+      const acc = addressAccounts[address]
+      return [
+        acc?.identity?.name,
+        address,
+        amount.toLocaleString(
+          'en-US',
+          { useGrouping: false, maximumFractionDigits: 20 },
+        )
+      ].join(',')
+    }).join('\n')
+  )
+
+  console.info(`Writing disbursal to: ${outputBase}.csv.`)
+  if(argv.dryRun) {
+    console.info('𝙳𝚛𝚢 𝚁𝚞𝚗: Not writing disbursal.')
+  } else {
+    await fs.writeFile(`${outputBase}.csv`, csv)
+  }
+
+  console.info({ total })
+
+  console.info(`Writing disbursal to: ${outputBase}.json.`)
+  if(argv.dryRun) {
+    console.info('𝙳𝚛𝚢 𝚁𝚞𝚗: Not writing disbursal.')
+  } else {
+    await fs.writeFile(
+      `${outputBase}.json`,
+      JSON.stringify(newMintAmounts, null, 2),
+    )
+  }
+})()
